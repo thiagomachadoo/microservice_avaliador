89af6c474222f643c0cc2466ea12c9efa91ec26d
Thiago da Silva Machado <thiagodasilvamachadoo44@gmail.com>
2023-04-03 21:05:51 -0300

    new changes

--- src/main/java/io/github/thiagomachadoo/microservice_avaliador/MicroserviceAvaliadorApplication.java
+++ src/main/java/io/github/thiagomachadoo/microservice_avaliador/MicroserviceAvaliadorApplication.java
@@ -2,12 +2,14 @@ package io.github.thiagomachadoo.microservice_avaliador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviceAvaliadorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceAvaliadorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceAvaliadorApplication.class, args);
+    }
 
 }
--- src/main/java/io/github/thiagomachadoo/microservice_avaliador/application/AvaliadorCreditoController.java
+++ src/main/java/io/github/thiagomachadoo/microservice_avaliador/application/AvaliadorCreditoController.java
@@ -1,2 +1,15 @@
-package io.github.thiagomachadoo.microservice_avaliador.application;public class AvaliadorCreditoController {
+package io.github.thiagomachadoo.microservice_avaliador.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("avaliacoes")
+public class AvaliadorCreditoController {
+
+    @GetMapping
+    public String status(){
+        return "OK!!";
+    }
 }
